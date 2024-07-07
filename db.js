a2cdc10126c1034855d7ccccaf4d4fa75cb47530
Rahulsahani18 <153098636+Rahulsahani18@users.noreply.github.com>
2024-07-07 23:45:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 
 const connectDB = async () => {
   try {
-     const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://rs7613718:*****@e-commerce-database.lejz8av.mongodb.net/refer", {
+     const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://rs7613718:Rahul1818@e-commerce-database.lejz8av.mongodb.net/refer", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // Other options if needed
