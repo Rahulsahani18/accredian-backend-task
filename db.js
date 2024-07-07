70f9c2b561a733453451e264e5206e04bc650aee
Rahulsahani18 <153098636+Rahulsahani18@users.noreply.github.com>
2024-07-07 23:19:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,9 @@ const colors = require('colors');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/refer", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Other options if needed
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/refer";
+     
+      // Other options if neede
     console.log(`Connected to MongoDB Database: ${conn.connection.host}`.bgMagenta.white);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`.bgRed.white);
